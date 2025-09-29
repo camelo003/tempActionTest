ee3f578ac912c763e0e6d8d5ccc29c686e150fd4
camelo003 <holysmoke003@gmail.com>
2025-09-29 03:13:38 -0300

    >:C

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   url: 'https://camelo003.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/tempActionTest/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
